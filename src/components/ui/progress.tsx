0e3ac5ee0e7e30c30b025e26986c8dd0102710b6
sw3205933776 <3205933776@qq.com>
2025-08-01 17:28:53 +0800

    style: update progress bar color

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -16,7 +16,8 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className={`h-full w-full flex-1 transition-all ${value === 100 ? "bg-black-100%" : "bg-black-50%"}`}
+      className={`h-full w-full flex-1 transition-all ${value === 100 ? "bg-progress-fill-complete" : "bg-progress-fill-default"}`}
+      // className={`h-full w-full flex-1 transition-all ${value === 100 ? "bg-black-100%" : "bg-black-50%"}`}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
