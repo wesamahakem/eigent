5742893957b51ee95cfef4e5d2ed899c5f0b4ca3
luo <luoyou1014@163.com>
2025-08-03 16:35:14 +0800

    fix: avoid cors error

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 api = FastAPI()
+api.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+)
